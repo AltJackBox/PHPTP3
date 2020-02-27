89d9331e7b37dd11935ad543c3eb492de5aef80a
mcreaper <mcreaper@hotmail.fr>
2020-02-27 15:23:14 +0100

    question 0 à 5 TMDB

--- Victor/TMDB/details.php
+++ Victor/TMDB/details.php
@@ -1,32 +1,36 @@
 <!-- récolte des informations de l'API en PHP -->
 
 <?php
+    require_once("utils.php");
+
     if (!isset($_GET['id_film']) || $_GET['id_film'] == '') {
         //On a pas le paramètre get pour l'id du film, on redirige donc l'user. 
         header('Location: page_de_detail.html');
         exit();
     }
 
-    //On récupère l'id du film
+    //On récupère l'id du film et ses infos en VO, EN, VF
     $id_film = $_GET['id_film'];
+    $movie_info = get_movie_info($id_film);
+    $lang = array(0 => "vo", 1 => "en", 2 => "vf");
 
-    //Le json associé (en français)
-    require_once('../tp3-helpers.php');
-    $movie_info_json = tmdbget("movie/".$id_film,["language"=>"fr"]);
-    $movie_info = json_decode($movie_info_json, true);
+    //On vérifie que le film existe bien, sinon on affiche un message d'erreur
+    if (isset($movie_info[$lang[0]]["status_code"]) && $movie_info[$lang[0]]["status_code"] == 34) {
+        echo("<b style='color:red'>Le film d'id $id_film n'existe pas !</b>");
+        exit();
+    }
     
-    //Comportement par défaut si la tagline n'existe aps
-    if (!isset($movie_info["tagline"])) $movie_info["tagline"] = "";
+    //Comportement par défaut si la tagline n'existe pas
+    if (!isset($movie_info[$lang[0]]["tagline"])) {
+        foreach($lang as $key => $value)
+            $movie_info[$value]["tagline"] = "";
+    }
 
-    //On sauvegardes les informations essentielles
-    $title = $movie_info["title"];
-    $original_title = $movie_info["original_title"];
-    $tagline = $movie_info["tagline"];
-    $overview = $movie_info["overview"];
-    $link = "https://www.themoviedb.org/movie/".$id_film;
+    //On met de côté les informations du json qui nous interesse :
+    $infos = array(0 => "title", 1 => "tagline", 2 => "overview", 3 => "link");
 
-    //Puis les informations supplémentaires.
-    $poster = 'https://image.tmdb.org/t/p/original'.$movie_info["poster_path"];
+    //Les informations supplémentaires.
+    $poster = 'https://image.tmdb.org/t/p/original'.$movie_info[$lang[0]]["poster_path"];
 ?>
 
 <!-- Affichage de la page avec les informations obtenues -->
@@ -35,22 +39,35 @@
     <head>
         <meta charset="utf-8">
         <title><?= $title ?></title>
-        <style type="text/css">
-
-        </style>
+        <link rel="stylesheet" href="details_style.css">
     </head>
 
     <body>
-        <!-- titre -->
-        <p><?=$title?></p>
-        <!-- titre original -->
-        <p><?=$original_title?></p>
-        <!-- tagline -->
-        <p><?=$tagline?></p>
-        <!-- description -->
-        <p><?=$overview?></p>
-        <!-- lien vers la page TMDB -->
-        <a href="<?=$link?>">lien vers TMDB</a>
+        <table>
+            <thead>
+                <tr> <th>VO</th> <th>EN</th> <th>VF</th> </tr>
+            </thead>
+            <tbody>
+                <?php
+                    foreach($infos as $index => $info_name) {
+                        echo("<tr>");
+                        foreach($lang as $key => $version) {
+                            echo("<td>");
+                            if ($info_name == "link") {
+                                $language = "";
+                                if ($version == "vf") $language = "fr-FR";
+                                if ($version == "en") $language = "en-EN";
+                                echo("<a href='https://www.themoviedb.org/movie/".$id_film."?language=".$language."'>".$movie_info[$version][$info_name]."voir l'affiche ici</a>");
+                            } else {
+                                echo("<p>".$movie_info[$version][$info_name]."</p>");
+                            }
+                            echo("</td>");
+                        }
+                        echo("</tr>");
+                    }
+                ?>
+            </tbody>
+        </table>
         <!-- affiche -->
         <img src="<?=$poster?>"/>
     </body>
