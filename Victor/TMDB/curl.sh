70407cd2479e839a606903515efa6252557903aa
Yael Para <yael.para@etu.univ-grenoble-alpes.fr>
2020-02-29 10:25:17 +0100

    Question 6 + Suppression dossier Victor

--- Victor/TMDB/curl.sh
+++
@@ -1,1 +0,0 @@
-curl "http://api.themoviedb.org/3/movie/550?api_key=ebb02613ce5a2ae58fde00f4db95a9c1&language=fr"
--- Victor/TMDB/exploration_CLI.php
+++
@@ -1,6 +0,0 @@
-<?php
-    //lien : http://api.themoviedb.org/3/movie/550?api_key=ebb02613ce5a2ae58fde00f4db95a9c1&language=fr
-    require_once('../tp3-helpers.php');
-    $movie_info_json = tmdbget("movie/550",["language=fr"]);
-    print_r($movie_info_json);
-?>
